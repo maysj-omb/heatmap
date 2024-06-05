369fb63b8feaa198f5428484f6c6ec28b830afde
John Mays <maysj@omb.nyc.gov>
2024-06-05 17:58:56 -0400

    initial commit w/o git

---
+++ README.md
@@ -0,0 +1,46 @@
+# Heatmap Script
+<img src="./assets/hpa_map.png" width="600" alt="Intensity map of central Manhattan, New York."/>
+
+The code in this repository is useful for producing a "heatmap" of geospatial data in Python.  It works by creating an image (2-D array of pixels) where each pixel represents a square of the coordinate space.
+
+## Installation:
+- Three libraries are used prominently:
+    - `matplotlib` (plotting)
+    - `pandas` (tabular data handling)
+    - `scipy` (only for its gaussian blur function)
+- `geopandas` can be helpful if you want to plot shapes alongside the heatmap or need to read geospatial data.
+- `ipykernel` needs to be installed in order to run the example notebook.
+
+Complete list of requirements is in `requirements.txt`.
+
+### Clone Project:
+```shell
+$ git clone https://github.com/maysj-omb/heatmap
+$ cd heatmap
+```
+
+### Install Libraries with `pip`:
+Honestly this code should be fairly version agnostic on Python3, but if you would like to clone my environment exactly:
+```shell
+heatmap $ pip install virtualenv
+heatmap $ virtualenv <-p <optional path to python 3.8>> .venv
+heatmap $ source .venv/bin/activate
+(.venv) heatmap $ pip install -r requirements.txt
+```
+
+## Use:
+See example notebook for use to get started.
+
+## Common Problems and F.A.Q.'s:
+- I am having trouble plotting geometries alongside my heatmap:
+    - This could be because they are not in the same [coordinate reference system](https://en.wikipedia.org/wiki/Spatial_reference_system)/map projection.
+    - This could also be an issue with `bounds`.  The heatmap image is rendered to the final figure using the matplotlib function `axes.imshow()` and the `extent` parameter of that function is set to the `bounds` tuple that *you* pass in, which is based on actual coordinates.  This is the only thing tying the image to the actuall coordinates on the map. Without `extent=bounds...`, your image is just a collection of pixels with no reference to the real world.  Try ensuring that the plot function is using the same `bounds` tuple for plotting the heatmap image and the geometries.
+- Most points are showing up as very light while only the very maxima show up prominently:
+    - Try increasing the `root` parameter so that lower values are mapped to higher colormap intensities (appear closer to the maxima).
+- My points are intense enough, but are very sparse:
+    - You can try several things: 
+        1. increase `area_size` so that more pixels appear with some color.
+        2. set `blur` to true.
+        3. if `blur` is true, try increasing `sigma` to increase the width of the blur.
+- I have found a bug or issue with the code:
+    - Contact the maintainer of this repository directly or create a GitHub issue [here](https://github.com/maysj-omb/heatmap/issues).
---
+++ heatmap.py
@@ -0,0 +1,71 @@
+# Author: John Mays | Policy & Operations Research @ OMB
+# maysj at-symbol omb dot nyc dot gov | Last Updated: 06/05/24
+
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from scipy.ndimage import gaussian_filter
+
+def create_granular_map(x_coords:pd.Series, y_coords:pd.Series, bounds:tuple,
+                        area_size:int, root:float=1, blur:bool=False,
+                        sigma:float=1) -> np.ndarray:
+    """
+    WARNING: make sure bounds, x_coords, and y_coords all belong to the same
+        coordinate ref system (CRS).
+    This function plots a map of calls as as an image of the NYC area with the
+    intensity of pixels reflecting the number of coordinates within their area.
+    Args:
+        x_coords, y_coords: two pandas series of the x & y coordinates to
+            constitute the image
+        bounds: a tuple containing the boundaries of the mapped area (x_min,
+            y_min, x_max, y_max)
+        area_size: the real-unit width of a square area represented by a pixel
+        root: the n-th root which scales the intensity map (1 = no scaling)
+        blur: choose whether or not to include a blur
+        sigma: the width of the gaussian blur in pixels
+    Returns:
+        intensity_map: numpy matrix of floats [0.0, 1.0] to reflect the
+            relative intensity of each square on the map
+    """
+    x_min, y_min, x_max, y_max = bounds
+    area_height = y_max - y_min
+    area_width = x_max-x_min
+    map_height = int(area_width/area_size)
+    map_width = int(area_height/area_size)
+    print(f'Resolution of map (width by height) will be: {map_width:.0f}'
+          f' by {map_height:.0f} {area_size} foot squares.')
+    # create empty matrix:
+    intensity_map = np.zeros((map_width, map_height), dtype=np.int64)
+
+    # remove from both series if one coordinate them is not a number/missing:
+    all_na = x_coords.isna() + y_coords.isna()
+    x_coords, y_coords = x_coords[~all_na], y_coords[~all_na]
+
+    # remove from both series if one coordinate is out-of-bounds (oob):
+    all_oob = (x_coords < x_min) + (x_coords > x_max) + (y_coords < y_min)\
+        + (y_coords > y_max)
+    x_coords, y_coords = x_coords[~all_oob], y_coords[~all_oob]
+
+    # perform broadcasted math & cast to int for simple binning:
+    x_coords = ((x_coords-x_min)/area_size).astype(int)
+    y_coords = ((y_coords-y_min)/area_size).astype(int)
+    missed_count = 0
+    for x, y in zip(x_coords, y_coords):
+        if y <= y_max and y <= y_min and x <= x_max and x <= x_min:
+            i, j = -y, x # have to mirror the placement due to matrix indexing
+            try:
+                intensity_map[i, j] += 1
+            except IndexError:
+                missed_count += 1
+        else:
+            missed_count += 1
+    print(f'{missed_count} coords were not within bounds.')
+    # convert to floats & normalize:
+    intensity_map = intensity_map.astype(float)/intensity_map.max()
+    # scale map:
+    intensity_map = np.power(intensity_map, (1/root))
+    # optional blur map:
+    if blur:
+        intensity_map = gaussian_filter(intensity_map, sigma=sigma)
+    return intensity_map
